95fb4b171263a5d47d5371ee2cdbe8f75f91c554
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-10-04 19:41:25 +0200

    Add basic layout

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+import style from './Footer.module.scss';
+
+export const Footer = () => (
+  <footer className={style.footer}>
+    <p>©</p>
+    <p>GitHub</p>
+  </footer>
+);
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+import style from './Header.module.scss';
+
+export const Header = () => (
+    <header className={style.container}>
+      <div className={style.title}>Tracker</div>
+    </header>
+  );
