e75bf960b2a3bc25d57f89f4af2402ae4c10d15e
Kamila Semotánová <126090107+KamilaSemotanova@users.noreply.github.com>
2023-10-14 12:05:38 +0200

    Add list of activities (#6)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,12 @@
+import localFont from 'next/font/local';
+import classnames from 'classnames';
+
 import style from './Header.module.scss';
+
+const kohoFont = localFont({ src: '../../fonts/KoHo/KoHo-Regular.woff2' });
 
 export const Header = () => (
   <header className={style.container}>
-    <div className={style.title}>tracker</div>
+    <div className={classnames(style.title, kohoFont.className)}>tracker</div>
   </header>
 );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,30 @@
+import '../styles/globalStyles.scss';
 import { type AppType } from 'next/app';
+import Head from 'next/head';
+import localFont from 'next/font/local';
 
 import { api } from '~/utils/api';
 
-import '~/styles/globalStyles.scss';
+const mulishFont = localFont({ src: '../fonts/Mulish/Mulish-Regular.woff2' });
 
 const MyApp: AppType = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+  <>
+    <style jsx global>{`
+      html {
+        font-family:
+          ${mulishFont.style.fontFamily},
+          -apple-system,
+          Cantarell,
+          Droid Sans,
+          Helvetica Neue;
+      }
+    `}</style>
+    <Head>
+      <title>Tracker</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Component {...pageProps} />
+  </>
 );
 
 export default api.withTRPC(MyApp);
