9445ce27972a219702fef4cca3aa0c25ffe9af24
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-10-31 09:25:02 +0100

    fix font size and label

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -3,11 +3,17 @@ import React, { InputHTMLAttributes } from 'react';
 import style from './TextField.module.scss';
 
 type TextFieldProps = {
+  label: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 } & InputHTMLAttributes<HTMLInputElement>;
 
-export const TextField: React.FC<TextFieldProps> = ({ onChange, ...props }) => (
+export const TextField: React.FC<TextFieldProps> = ({
+  onChange,
+  label,
+  ...props
+}) => (
   <label>
+    <span className={style.label}> {label} </span>
     <input className={style.input} onChange={onChange} {...props} />
   </label>
 );
