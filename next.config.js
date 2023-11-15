ab6bee094913b0381f283ffb6dd58b27436c454a
Lukas Cizek <globus303@gmail.com>
2023-11-15 12:34:33 +0100

    Try some more

--- next.config.js
+++ next.config.js
@@ -13,7 +13,10 @@ const config = {
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'http://localhost:3000,https://*.vercel.app',
+          },
           {
             key: 'Access-Control-Allow-Methods',
             value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -2,11 +2,32 @@ import { createNextApiHandler } from '@trpc/server/adapters/next';
 
 import { appRouter } from '../../../../server/root';
 import { createTRPCContext } from '../../../../server/context';
-import { withCors } from '../../../utils/trpc';
 
-export default withCors(
-  createNextApiHandler({
-    router: appRouter,
-    createContext: createTRPCContext,
-  }),
-);
+export default createNextApiHandler({
+  router: appRouter,
+  createContext: createTRPCContext,
+  responseMeta(ctx) {
+    ctx.ctx?.res?.setHeader(
+      'Access-Control-Allow-Origin',
+      'http://localhost:3000,https://*.vercel.app',
+    );
+    ctx.ctx?.res?.setHeader('Access-Control-Allow-Credentials', 'true');
+    ctx.ctx?.res?.setHeader(
+      'Access-Control-Allow-Methods',
+      'OPTIONS, GET, POST, DELETE, PUT, PATCH',
+    );
+    ctx.ctx?.res?.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+    );
+
+    if (ctx.ctx?.req?.method === 'OPTIONS') {
+      ctx.ctx?.res?.writeHead(200);
+    }
+
+    return {
+      headers: ctx.ctx?.res?.getHeaders() as Record<string, string>,
+      statusCode: ctx.ctx?.res?.statusCode || 200,
+    };
+  },
+});
