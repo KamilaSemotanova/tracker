a07520b51fc801f6ca37ddf36c964ff6006f442b
Lukas Cizek <globus303@gmail.com>
2023-11-15 13:10:50 +0100

    Fix cors

--- next.config.js
+++ next.config.js
@@ -7,6 +7,29 @@ await import('./src/env.js');
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'authorization,x-csrf-token,x-requested-with,accept,accept-version,content-length,content-md5,content-type,date,x-api-version',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default config;
