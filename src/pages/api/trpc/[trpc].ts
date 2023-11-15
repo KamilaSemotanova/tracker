31edda9e431345fe6617b87baa71d65f33189ef2
Lukas Cizek <globus303@gmail.com>
2023-11-15 13:15:39 +0100

    fix stuff

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,9 +1,34 @@
 import { createNextApiHandler } from '@trpc/server/adapters/next';
+import { NextApiRequest, NextApiResponse } from 'next';
 
 import { appRouter } from '../../../../server/root';
 import { createTRPCContext } from '../../../../server/context';
 
-export default createNextApiHandler({
-  router: appRouter,
-  createContext: createTRPCContext,
-});
+const allowCors =
+  (fn: any) => async (req: NextApiRequest, res: NextApiResponse) => {
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+    );
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+    );
+
+    if (req.method === 'OPTIONS') {
+      res.status(200).end();
+
+      return;
+    }
+
+    return await fn(req, res);
+  };
+
+export default allowCors(
+  createNextApiHandler({
+    router: appRouter,
+    createContext: createTRPCContext,
+  }),
+);
