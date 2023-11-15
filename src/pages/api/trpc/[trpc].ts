63ec4e58ebe81a265eae8354578c041e35a2f129
Lukas Cizek <globus303@gmail.com>
2023-11-15 12:13:15 +0100

    Fix cors (#15)

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -6,4 +6,22 @@ import { createTRPCContext } from '../../../../server/context';
 export default createNextApiHandler({
   router: appRouter,
   createContext: createTRPCContext,
+  responseMeta(ctx) {
+    ctx.ctx?.res?.setHeader('Access-Control-Allow-Origin', '*');
+    ctx.ctx?.res?.setHeader('Access-Control-Request-Method', '*');
+    ctx.ctx?.res?.setHeader(
+      'Access-Control-Allow-Methods',
+      'OPTIONS, GET, POST, DELETE, PUT, PATCH',
+    );
+    ctx.ctx?.res?.setHeader('Access-Control-Allow-Headers', '*');
+
+    if (ctx.ctx?.req?.method === 'OPTIONS') {
+      ctx.ctx?.res?.writeHead(200);
+    }
+
+    return {
+      headers: ctx.ctx?.res?.getHeaders() as Record<string, string>,
+      statusCode: ctx.ctx?.res?.statusCode || 200,
+    };
+  },
 });
