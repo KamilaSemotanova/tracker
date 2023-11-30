afe06907a908f5ee10951307e31c2be81cdf69fa
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-11-30 21:25:42 +0100

    Fix eslint issues

--- src/components/ChangeData/ChangeData.tsx
+++ src/components/ChangeData/ChangeData.tsx
@@ -1,21 +1,58 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useState, useEffect } from 'react';
+import classnames from 'classnames';
+import { useRouter } from 'next/router';
 
-// import style from './ChangeData.module.scss';
+import { trpc } from '../../utils/trpc';
+import style from './ChangeData.module.scss';
 
 export const ChangeData = () => {
   const [profileState, setProfileState] = useState({});
-  const [passwordState, setPasswordState] = useState({});
+  const [updatedPassword, setUpdatedPassword] = useState('');
+  const [updatedPasswordControl, setUpdatedPasswordControl] = useState('');
+  const [warningMessagePassword, setWarningMessagePassword] = useState('');
+  const [warningMessageProfile, setWarningMessageProfile] = useState('');
+  const [isVisible, setIsVisible] = useState(true);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(false);
+    }, 10000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  // const updateProfile = trpc.user.updateUser.useMutation({
+  //   onSuccess: () => {
+  //     setWarningMessageProfile('Profil byl upraven');
+  //   },
+  //   onError: () => {
+  //     setWarningMessageProfile('Úprava se nezdařila');
+  //   },
+  // });
+
+  // const updatePassword = trpc.user.updatePassword.useMutation({
+  //   onSuccess: () => {
+  //     setWarningMessagePassword('Heslo bylo změněno');
+  //   },
+  //   onError: () => {
+  //     setWarningMessagePassword('Úprava se nezdařila');
+  //   },
+  // });
 
   const handleSubmitProfile = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
-    // console.log(profileState);
   };
 
   const handleSubmitPassword = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    // console.log(passwordState);
+    if (updatedPassword !== updatedPasswordControl) {
+      setWarningMessagePassword('hesla se neshodují');
+    }
   };
 
   const handleChangeProfile = (e: ChangeEvent<HTMLInputElement>) => {
@@ -23,41 +60,83 @@ export const ChangeData = () => {
   };
 
   const handleChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.id) {
-      setPasswordState({ ...passwordState, [e.target.id]: e.target.value });
+    if (e.target.id === 'password') {
+      setUpdatedPassword(e.target.value);
+    }
+
+    if (e.target.id === 'controlPassword') {
+      setUpdatedPasswordControl(e.target.value);
     }
   };
 
+  const handleReturn = () => {
+    router.push('/');
+  };
+
   return (
-    <section>
-      <h2>Upravit profil</h2>
-      <form onSubmit={handleSubmitProfile}>
-        <label>
-          Jméno
-          <input type="text" id="name" onChange={handleChangeProfile} />
-        </label>
-        <label>
-          E-mail
-          <input type="text" id="email" onChange={handleChangeProfile} />
-        </label>
-        <button type="submit">Uložit</button>
-      </form>
-      <h2>Změnit heslo</h2>
-      <form onSubmit={handleSubmitPassword}>
-        <label>
-          Heslo
-          <input type="text" id="password" onChange={handleChangePassword} />
-        </label>
-        <label>
-          Kontrola hesla
-          <input
-            type="text"
-            id="controlPassword"
-            onChange={handleChangePassword}
-          />
-        </label>
-        <button type="submit">Uložit</button>
-      </form>
+    <section className={style.changeForm}>
+      <div className={style.forms}>
+        <div className={style.formBox}>
+          <h2 className={style.title}>Upravit profil</h2>
+          <form className={style.form} onSubmit={handleSubmitProfile}>
+            <label className={style.label}>
+              Jméno
+              <input
+                className={style.input}
+                type="text"
+                id="name"
+                onChange={handleChangeProfile}
+              />
+            </label>
+            <label className={style.label}>
+              E-mail
+              <input
+                className={style.input}
+                type="text"
+                id="email"
+                onChange={handleChangeProfile}
+              />
+            </label>
+            {isVisible && <p>{warningMessageProfile}</p>}
+            <button type="submit" className={style.button}>
+              Uložit
+            </button>
+          </form>
+        </div>
+        <div className={style.formBox}>
+          <h2 className={style.title}>Změnit heslo</h2>
+          <form className={style.form} onSubmit={handleSubmitPassword}>
+            <label className={style.label}>
+              Heslo
+              <input
+                className={style.input}
+                type="text"
+                id="password"
+                onChange={handleChangePassword}
+              />
+            </label>
+            <label className={style.label}>
+              Kontrola hesla
+              <input
+                className={style.input}
+                type="text"
+                id="controlPassword"
+                onChange={handleChangePassword}
+              />
+            </label>
+            {isVisible && <p>{warningMessagePassword}</p>}
+            <button type="submit" className={style.button}>
+              Uložit
+            </button>
+          </form>
+        </div>
+      </div>
+      <button
+        className={classnames(style.button, style.buttonBack)}
+        onClick={handleReturn}
+      >
+        Zpět
+      </button>
     </section>
   );
 };
