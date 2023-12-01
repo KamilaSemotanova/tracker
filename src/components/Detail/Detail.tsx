350b08501244dc7e28bce2c57d1586a9845a41b1
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-12-01 18:19:55 +0100

    Finalize functionality behind detail

--- src/components/Detail/Detail.tsx
+++ src/components/Detail/Detail.tsx
@@ -3,30 +3,79 @@ import classnames from 'classnames';
 
 import { Row } from '../Row/Row';
 import { trpc } from '../../utils/trpc';
-import { StreekCounter } from './StrikeCounter';
 import style from './Detail.module.scss';
 
 export const DetailOfActivity = () => {
   const router = useRouter();
   const { id } = router.query;
 
+  const utils = trpc.useContext();
+
   const activityData = trpc.activities.read.useQuery({ id: Number(id) }).data;
+  const updateDoneCounter = trpc.activities.updateDoneCounter.useMutation({
+    onSuccess: () => {
+      utils.activities.invalidate();
+    },
+  });
+
+  const deleteActivity = trpc.activities.delete.useMutation({
+    onSuccess: () => {
+      utils.activities.invalidate();
+    },
+  });
+
+  const handleDelete = () => {
+    if (activityData) {
+      deleteActivity.mutate({ id: activityData.id });
+    }
+
+    router.push('/');
+  };
+
+  const handleActivityClick = ({
+    activityId,
+    timesDone,
+  }: {
+    activityId: number;
+    timesDone: number;
+  }) => {
+    updateDoneCounter.mutate({ id: activityId, timesDone: timesDone + 1 });
+  };
 
   return (
     activityData && (
       <Row flexCol fullWidth justifyCenter itemsCenter>
         <div className={style.box}>
-          <h1
-            className={classnames(style.activity, {
-              [style.zero]: activityData.timesDone === 0,
-            })}
+          <nav className={style.navigation}>
+            <button onClick={() => router.push('/')} className={style.back} />
+            <h1
+              className={classnames(style.activity, {
+                [style.zero]: activityData.timesDone === 0,
+              })}
+            >
+              {activityData.name}
+            </h1>
+            <button onClick={handleDelete} className={style.delete} />
+          </nav>
+          <button
+            aria-label={`Dokončena aktivita ${activityData.name}.`}
+            type="button"
+            onClick={() => {
+              handleActivityClick({
+                activityId: activityData.id,
+                timesDone: activityData.timesDone,
+              });
+            }}
           >
-            {activityData?.name}
-          </h1>
-          <StreekCounter dayStreek={activityData.timesDone} />
-          <div>calendar</div>
-          <button>delete</button>
-          <button onClick={() => router.push('/')}>Dashboard</button>
+            done
+          </button>
+          <div className={style.container}>
+            <div className={style.counter}>
+              <p className={style.number}>{activityData.timesDone}</p>
+            </div>
+            <p>dní</p>
+          </div>
+          {/* <div>calendar</div> */}
         </div>
       </Row>
     )
