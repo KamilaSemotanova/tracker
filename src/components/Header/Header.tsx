b72fd0cf05658d3a9f0a09fc61cc74942fe9bbf7
Kamila Semotánová <kamila.semotanova@gmail.com>
2024-01-11 14:51:12 +0100

    Implement change

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,22 @@
 import localFont from 'next/font/local';
 import classnames from 'classnames';
+import { useRouter } from 'next/router';
 
 import style from './Header.module.scss';
 
 const kohoFont = localFont({ src: '../../fonts/KoHo/KoHo-Regular.woff2' });
 
-export const Header = () => (
-  <header className={style.container}>
-    <div className={classnames(style.title, kohoFont.className)}>tracker</div>
-  </header>
-);
+export const Header = () => {
+  const router = useRouter();
+
+  return (
+    <header className={style.container}>
+      <div
+        className={classnames(style.title, kohoFont.className)}
+        onClick={() => router.push('/')}
+      >
+        tracker
+      </div>
+    </header>
+  );
+};
